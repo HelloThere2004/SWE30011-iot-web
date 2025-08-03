aa2467120d996d5a7b884b57eb0bed718c5fea45
HelloThere2004 <binhcanguyen04@gmail.com>
2025-08-03 16:46:59 +0700

    Third Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,13 @@ export default function Home() {
           title="IoT Dashboard"
         ></iframe>
       </main>
+      <div className="flex justify-center items-center my-5">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-auto">
+        Turn On/Off System
+    </button>
+        </div>
+      
     </div>
+    
   );
 }
